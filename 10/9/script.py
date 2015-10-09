fc4fbb93af945cc78e0349ee69b562355f574714
Konrad Iturbe <github@chernowii.com>
2015-10-09 14:32:10 +0000

    Create script.py

---
+++ 10/9/script.py
@@ -0,0 +1,17 @@
+import time
+a=0
+while a<10:
+ a=a+1
+ print(a)
+#Mine
+time.sleep(5)
+b=0
+while b<15:
+ b=b+1
+ print(b)
+time.sleep(5)
+#Range example
+c=0
+for c in range(15):
+ c=c+1
+ print(c)
