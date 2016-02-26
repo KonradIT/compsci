fe353a3bf07a3a170420191e78a0f6d77b56ea0b
Konrad Iturbe <github@chernowii.com>
2016-02-26 16:25:23 +0100

    Added a counter

--- 02/25/game.py
+++ 02/25/game.py
@@ -1,13 +1,16 @@
 #The game
 import random
+count = 0
 def game():
   randomInt=random.randint(1,9)
-  print("Guess the number!")
+  global count
+  print("Guess the number!\nNumber of tries:",count)
   guess=int(input("user@host: "))
   if (randomInt==guess):
     print("Congratulations!\nYou have a prize waiting!\nJust give your Western Union account to the Prince of Nigeria who will personally email you now")
   else:
     print("Uh, try again!")
+    count=count+1
     game()
     
 if __name__ == "__main__":
