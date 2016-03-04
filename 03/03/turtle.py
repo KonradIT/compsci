9fd87fc6039d06cf52493e67454b4a2bc4199761
Konrad Iturbe <github@chernowii.com>
2016-03-04 16:40:51 +0100

    Update turtle.py

--- 03/03/turtle.py
+++ 03/03/turtle.py
@@ -7,22 +7,23 @@ yellow = turtle.Turtle()
 
 green.color("green")
 green.forward(100) 
-green.left(90) 
+green.left(270) 
 green.forward(100) 
 
 red.color("red")
 red.left(90)  
 red.forward(100)
-red.left(90)
+red.left(270)
 red.forward(100)
 
 blue.color("blue")
 blue.backward(100)
-blue.right(90)
+blue.right(270)
 blue.forward(100)
 
 yellow.color("yellow")
 yellow.left(270)
 yellow.forward(100)
-yellow.right(270)
+yellow.right(90)
 yellow.forward(100)
+
