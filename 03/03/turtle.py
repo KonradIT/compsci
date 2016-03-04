0deee33686b613a7d9a1562263123705993ff446
Konrad Iturbe <github@chernowii.com>
2016-03-04 16:38:31 +0100

    Create turtle.py

---
+++ 03/03/turtle.py
@@ -0,0 +1,28 @@
+#Had to do it for science
+import turtle
+green = turtle.Turtle()
+red = turtle.Turtle()
+blue = turtle.Turtle()
+yellow = turtle.Turtle()
+
+green.color("green")
+green.forward(100) 
+green.left(90) 
+green.forward(100) 
+
+red.color("red")
+red.left(90)  
+red.forward(100)
+red.left(90)
+red.forward(100)
+
+blue.color("blue")
+blue.backward(100)
+blue.right(90)
+blue.forward(100)
+
+yellow.color("yellow")
+yellow.left(270)
+yellow.forward(100)
+yellow.right(270)
+yellow.forward(100)
