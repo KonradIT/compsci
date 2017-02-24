04ec52266796491a22933848a356df598208ee8f
Konrad Iturbe <github@chernowii.com>
2017-02-24 15:41:19 +0100

    Add challenge11

---
+++ python-exercises/challenge11.py
@@ -0,0 +1,23 @@
+###Battle soldiers game:
+
+PLAYER_1_MONEY=10
+PLAYER_2_MONEY=10
+CURRENT_PLAYER=1
+class Game():
+    def __init__(self):
+        self._money=0
+    def battle(self):
+        print("    BATTLE!")
+    def build(self):
+        print("    BUILD!")
+    def print_screen(self, player):
+        CURRENT_PLAYER_MONEY=0
+        x="PLAYER_" + str(player) + "_MONEY"
+        CURRENT_PLAYER_MONEY = globals()[x]
+        print("Welcome to the battle field, player " + str(CURRENT_PLAYER) +"\nYou're battling againts player " + str(0 if player==1 else 1) + ".")
+        print("Let's see your options:\n    - [0] Attack the Enemy.\n" + str("    - [1] Build your Military Base" if CURRENT_PLAYER_MONEY > 20 else "    You need at least 20 USD to build an army."))
+        print("    Your money: " + str(CURRENT_PLAYER_MONEY))
+        CHOICE=int(input("    Enter your choice: "))
+        self.battle() if CHOICE == 0 else self.build()
+
+Game().print_screen(CURRENT_PLAYER)
