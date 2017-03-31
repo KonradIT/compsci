08fc59fed88425c28d2d9ef76db4c863af180021
Konrad Iturbe <github@chernowii.com>
2017-03-31 15:00:49 +0200

    Update challenge13.py

--- python-exercises/challenge13.py
+++ python-exercises/challenge13.py
@@ -1,9 +1,10 @@
-#ATM machine
-Amount=int(input("Enter amount to withdraw: "))
-FiftyDollar=0
-TwentyDollar=0
-TenDollar=0
-Temp=0
-print(Amount % 5)
-print(Amount % 20)
-print(Amount % 10)
+withdrawal = int(input("Money to withdraw)"))
+if withdrawal % 10 == 0:
+  Fifties = withdrawal // 50
+  print (str(Fifties) + " $50 notes")
+  Twenties = withdrawal % 50 // 20
+  print (str(Twenties) + " $20 notes")
+  Tens = withdrawal % 50 % 20 // 10
+  print (str(Tens) + " $10 notes")
+else:
+  print("You did not enter a multiple of 10")
