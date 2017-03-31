7355c7532e21c66054201a95d03ab9397b7dc46c
Konrad Iturbe <github@chernowii.com>
2017-03-31 15:19:14 +0200

    Update challenge13.py

--- python-exercises/challenge13.py
+++ python-exercises/challenge13.py
@@ -1,10 +1,23 @@
-withdrawal = int(input("Money to withdraw: "))
-if withdrawal % 10 == 0:
-  Fifties = withdrawal // 50
-  print (str(Fifties) + " $50 notes")
-  Twenties = withdrawal % 50 // 20
-  print (str(Twenties) + " $20 notes")
-  Tens = withdrawal % 50 % 20 // 10
-  print (str(Tens) + " $10 notes")
+import sys
+print("CASH MACHINE")
+amt_to_withdraw = 0
+if not str(sys.argv[1]) == "":
+      amt_to_withdraw=int(sys.argv[1])
 else:
-  print("You did not enter a multiple of 10")
+      amt_to_withdraw = int(input("Money to withdraw: "))
+if amt_to_withdraw % 10 == 0 and amt_to_withdraw <= 500:
+      bill_50 = amt_to_withdraw // 50
+      for i in range(bill_50):
+            print ("=	[  50  ] ")
+            print ("	" + str(bill_50) + " notes.")
+      bill_20 = amt_to_withdraw % 50 // 20
+      for i in range(bill_20):
+            print ("=	[  20  ] ")
+            print("	" + str(bill_20) + " notes.")
+      bill_10 = amt_to_withdraw % 50 % 20 // 10
+      for i in range(bill_10):
+            print ("=	[  10  ] ")
+            print("	" + str(bill_10) + " notes.")
+else:
+      print("There is an error.")
+      print("Maximum amount is $500") if amt_to_withdraw > 500 else print("Only multiples of 10")
