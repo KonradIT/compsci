968b0e463fced465aef7d7b65b3acea039c32f1c
Konrad Iturbe <github@chernowii.com>
2017-03-31 18:20:06 +0400

    Update challenge14.py

--- python-exercises/challenge14.py
+++ python-exercises/challenge14.py
@@ -1,10 +1,19 @@
-print("THE INCREDIBLE GAME")
-board=[
-	["", "", ""],
-	["", "", ""],
-	["", "", ""]
-]
-def display_board():
-	for i in board:
-		print(str(i).replace(",",""))
-display_board()
+import random
+board=[[" ", " ", " ", " "],[" ", " ", " ", " "],[" ", " ", " ", " "],[" ", " ", " ", " "],[" ", " ", " ", " "]]
+pl=["Player","Knight"]
+game_over=False
+def display_board(knight_pos, player_pos):
+    board[knight_pos.index("K")][knight_pos.index("K")] = " "
+    board[player_pos[0]][player_pos[1]] = "P"
+    board[knight_pos[0]][knight_pos[1]] = "K"
+    if board[knight_pos[0]][knight_pos[1]] == board[player_pos[0]][player_pos[1]]:
+        print("Game over")
+        game_over=True
+    else:
+        for col in board:
+            print(str(col).replace(",",""))
+while game_over == False:
+    for i in pl:
+        player=input(i + ": Enter your move: ")
+        display_board([random.randint(0,3),random.randint(0,3)],[1,3])
+        
