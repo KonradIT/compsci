4bf3c6a934bc282ae8063bbb156ba1356a23b19c
Konrad Iturbe <github@chernowii.com>
2017-04-04 16:38:25 +0200

    Update challenge14.py

--- python-exercises/challenge14.py
+++ python-exercises/challenge14.py
@@ -1,19 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import random
-board=[[" ", " ", " ", " "],[" ", " ", " ", " "],[" ", " ", " ", " "],[" ", " ", " ", " "],[" ", " ", " ", " "]]
-pl=["Player","Knight"]
-game_over=False
-def display_board(knight_pos, player_pos):
-    board[knight_pos.index("K")][knight_pos.index("K")] = " "
-    board[player_pos[0]][player_pos[1]] = "P"
-    board[knight_pos[0]][knight_pos[1]] = "K"
-    if board[knight_pos[0]][knight_pos[1]] == board[player_pos[0]][player_pos[1]]:
-        print("Game over")
-        game_over=True
+board = [[' ', ' ', ' ', ' ',' '], [' ', ' ', ' ', ' ',' '], [' ', ' ', ' ', ' ',' '], [' ', ' ', ' ', ' ',' '], [' ', ' ', ' ', ' ',' ']]
+pl = ['Player', 'Knight']
+game_over = False
+v_player_data = [[], []] #First array: coordinates
+
+def display_board(player_pos):
+	for col in board:
+		print(str(col).replace(',', ''))
+
+
+def player_data(player, data=''):
+    if data == '':
+        # Get player data
+        return v_player_data[player]
     else:
-        for col in board:
-            print(str(col).replace(",",""))
+        # Set player data
+        v_player_data[player] = data
+
 while game_over == False:
     for i in pl:
-        player=input(i + ": Enter your move: ")
-        display_board([random.randint(0,3),random.randint(0,3)],[1,3])
-        
+        player = input(i + ': Enter your move [U/D/L/R]: ').upper()
+        if player == 'U':
+            print("UP")
+        elif player == 'D':
+            print("DOWN")
+        elif player == 'L':
+            print("LEFT")
+        elif player == 'R':
+            print("RIGHT")
+        last_coordinates = [1, 2]
+        display_board(i)
