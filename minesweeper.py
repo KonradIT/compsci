dc4f1373eb2611702c6939736eab225f2029c87d
konradit <konradit@users.noreply.github.com>
2017-10-05 22:00:46 +0200

    Minesweeper

--- minesweeper.py
+++ minesweeper.py
@@ -10,15 +10,11 @@ class MineSweeper:
 		self.__w=w
 		self.__h=h
 		self.__Board = [["." for x in range(w)] for y in range(h)]
-		for i in self.__Board:
-			print(str(i).replace(",","").replace("[","").replace("]","").replace("\'",""))
+		self.print_board()
 		if w == 8:
 			for _ in range(int(w/2)):
 				self.__Board[random.randint(0,w-1)][random.randint(0,h-1)] = "t"
 		if w == 16:
-			for _ in range(int(w/2)):
-				self.__Board[random.randint(0,w-1)][random.randint(0,h-1)] = "t"
-		if w == 30:
 			for _ in range(int(w/2)):
 				self.__Board[random.randint(0,w-1)][random.randint(0,h-1)] = "t"
 
@@ -28,32 +24,90 @@ class MineSweeper:
 		for i in self.__Board:
 			count += 1
 			arr.append(count)
-		print("   " + str(arr).replace(",","").replace("[","").replace("]","").replace(" ", "  "))
+		arr2=[]
+		for i in arr:
+			if len(str(i)) == 1:
+				arr2.append(str(i)+" ")
+			else:
+				arr2.append(str(i)+"")	
+		print("   " + str(arr2).replace(",","").replace("[","").replace("]","").replace("'",""))
 		for index, i in enumerate(self.__Board):
-			print(str(index+1) + "  " + str(i).replace(",","").replace("[","").replace("]","").replace("\'","").replace("t",".").replace(" ", "  "))
+			if len(str(index+1)) == 1:
+				print(str(index+1) + "  " + str(i).replace(",","").replace("[","").replace("]","").replace("\'","").replace("t",".").replace(" ", "  "))
+			else:
+				print(str(index+1) + " " + str(i).replace(",","").replace("[","").replace("]","").replace("\'","").replace("t",".").replace(" ", "  "))
 
 	def shoot(self):
-		shoot_w = int(input(">>> W: "))-1
-		shoot_h = int(input(">>> H: "))-1
+		dots = 0
+		for i in self.__Board:
+			for i2 in i:
+				if i2 == ".":
+					dots += 1
+		if dots == 0:
+			print("You won!")
+			exit()
+		shoot_w = int(input(">>> H: "))-1
+		shoot_h = int(input(">>> W: "))-1
 		if self.__Board[shoot_w][shoot_h] == "t":
 			print("Game over.")
 			exit()
+		if self.__Board[shoot_w][shoot_h] == "O":
+			print("Square already blown up!")
+			self.shoot()
 		else:
-			if self.__w == 8:
-				for _ in range(int(self.__w/2)):
-					self.__Board[random.randint(0,self.__w-1)][random.randint(0,self.__h-1)] = "O"
-			if self.__w == 16:
-				for _ in range(int(self.__w/2)):
-					self.__Board[random.randint(0,self.__w-1)][random.randint(0,self.__h-1)] = "O"
-			if self.__w == 30:
-				for _ in range(int(self.__w/2)):
-					self.__Board[random.randint(0,self.__w-1)][random.randint(0,self.__h-1)] = "O"
+			self.__Board[shoot_w][shoot_h] = "O"
+			for i in range(2):
+				print(i)
+				i = i + 1
+				if shoot_w == 7:
+					i = 0
+				if shoot_h == 7:
+					i = 0
+				if self.__Board[shoot_w][shoot_h-i] == ".":
+					#print("debug")
+					self.__Board[shoot_w][shoot_h-i] = "O"
+				if self.__Board[shoot_w][shoot_h+i] == ".":
+					#print("debug")
+					self.__Board[shoot_w][shoot_h+i] = "O"
+				if self.__Board[shoot_w-i][shoot_h] == ".":
+					#print("debug")
+					self.__Board[shoot_w-i][shoot_h] = "O"
+				if self.__Board[shoot_w+i][shoot_h] == ".":
+					#print("debug")
+					self.__Board[shoot_w+i][shoot_h] = "O"
+				if self.__Board[shoot_w+i][shoot_h+i] == ".":
+					#print("debug")
+					self.__Board[shoot_w+i][shoot_h+i] = "O"
+				if self.__Board[shoot_w-i][shoot_h-i] == ".":
+					#print("debug")
+					self.__Board[shoot_w-i][shoot_h-i] = "O"
+				if self.__Board[shoot_w-i][shoot_h+i] == ".":
+					#print("debug")
+					self.__Board[shoot_w-i][shoot_h+i] = "O"
+				if self.__Board[shoot_w+i][shoot_h-i] == ".":
+					#print("debug")
+					self.__Board[shoot_w+i][shoot_h-i] = "O"
 			self.print_board()
 			
 	
 	def cheat(self):
+		count=0
+		arr=[]
 		for i in self.__Board:
-			print(str(i).replace(",","").replace("[","").replace("]","").replace("\'",""))
+			count += 1
+			arr.append(count)
+		arr2=[]
+		for i in arr:
+			if len(str(i)) == 1:
+				arr2.append(str(i)+" ")
+			else:
+				arr2.append(str(i)+"")	
+		print("   " + str(arr2).replace(",","").replace("[","").replace("]","").replace("'",""))
+		for index, i in enumerate(self.__Board):
+			if len(str(index+1)) == 1:
+				print(str(index+1) + "  " + str(i).replace(",","").replace("[","").replace("]","").replace("\'","").replace(" ", "  "))
+			else:
+				print(str(index+1) + " " + str(i).replace(",","").replace("[","").replace("]","").replace("\'","").replace(" ", "  "))
 
 	
 	def select_dificulty(self):
@@ -65,11 +119,9 @@ class MineSweeper:
 		print("                                           |_|      ")
 
 		print("C H O O S E Y O U R D I F I C U L T Y L E V E L")
-		print("\t[ ] 1 - Easy (8x8)\n\t[ ] 2 - Medium (16x16)\n\t[ ] 3 - Hard (30x10)")
+		print("\t[ ] 1 - Easy (8x8)\n\t[ ] 2 - Medium (16x16)")
 		choice=int(input("\t>>> "))
 		if choice == 1:
 			self.start_game(8,8)
 		if choice == 2:
 			self.start_game(16,16)
-		if choice == 3:
-			self.start_game(30,10)
