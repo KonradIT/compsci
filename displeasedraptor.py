d201f42fa6d075b0523c4f5cb078179476b08dc1
Konrad Iturbe <KonradIT@users.noreply.github.com>
2017-10-27 11:44:31 +0200

    Update displeasedraptor.py

--- displeasedraptor.py
+++ displeasedraptor.py
@@ -1,7 +1,13 @@
-class Pig():
-	def __init__(self, xpos, ypos):
-		print("The pig")
-
+import random
+class Raptor():
+	def __init__(self):
+		print(">>> Attacks the swine")
+		self.Lost = False
+		self.Moves = []
+	def addMoves(self):
+		self.Moves.append(move)
+	def getMoves(self):
+		return self.Moves
 
 class Grid():
 	def __init__(self):
@@ -14,8 +20,29 @@ class Grid():
 		print("              | |                                             | |                  ")
 		print("              |_|                                             |_|                  ")
 		self.TheGrid=[["." for x in range(10)] for y in range(6)]
+		raptorpos=[]
+		self.TheGrid[random.randint(0,5)][random.randint(0,9)] = "T"
+		self.TheGrid[random.randint(0,5)][random.randint(0,9)] = "S"
+		raptorpos = [random.randint(0,5),random.randint(0,9)]
+		self.TheGrid[raptorpos[0]][raptorpos[1]] = "R"
+
 	def printgrid(self):
 		for i in self.TheGrid:
-			print(str(i).replace(",","").replace("[","").replace("]","").replace("\'",""))
+			print("   " + str(i).replace(",","").replace("[","").replace("]","").replace("\'",""))
+	def moveRaptor(self,direction):
+		for i in direction:
+			if direction == "U":
+				value_x=raptorpos[0]
+				value_y=raptorpos[1]
+				raptorpos=[value_x-1,value_Y]
+				self.TheGrid[raptorpos[0]][raptorpos[1]] = "R"
+				self.printgrid()
 grid = Grid()
+raptor = Raptor()
 grid.printgrid()
+while raptor.Lost == False:
+	moves=int(input(">>> How many moves: "))
+	for _ in range(moves):
+		raptor.moves(input(">>> Move L/R/U/D: ").upper())
+		raptor.moves()
+	grid.moveRaptor(raptor.moves())
