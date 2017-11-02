2d0201212e58ab56f9f3c688924adba86ed0a1f5
konradit <konradit@users.noreply.github.com>
2017-11-02 19:57:50 +0100

    Displeased Raptor

--- displeasedraptor.py
+++ displeasedraptor.py
@@ -1,13 +1,15 @@
 import random
 class Raptor():
 	def __init__(self):
-		print(">>> Attacks the swine")
+		print(">>> Attacks the swine\n>>> R: Raptor (You)\n>>> T: TNT (AVOID!)\n>>> S: Swine")
 		self.Lost = False
 		self.Moves = []
-	def addMoves(self):
+	def addMoves(self, move):
 		self.Moves.append(move)
 	def getMoves(self):
 		return self.Moves
+	def clearMoves(self):
+		self.Moves = []
 
 class Grid():
 	def __init__(self):
@@ -19,30 +21,80 @@ class Grid():
 		print(" |_____/|_|___/ .__/|_|\___|\__,_|___/\___|\__,_| |_|  \_\__,_| .__/ \__\___/|_|   ")
 		print("              | |                                             | |                  ")
 		print("              |_|                                             |_|                  ")
+
 		self.TheGrid=[["." for x in range(10)] for y in range(6)]
-		raptorpos=[]
+		self.raptorpos=[]
 		self.TheGrid[random.randint(0,5)][random.randint(0,9)] = "T"
 		self.TheGrid[random.randint(0,5)][random.randint(0,9)] = "S"
-		raptorpos = [random.randint(0,5),random.randint(0,9)]
-		self.TheGrid[raptorpos[0]][raptorpos[1]] = "R"
+		self.raptorpos = [random.randint(0,5),random.randint(0,9)]
+		self.TheGrid[self.raptorpos[0]][self.raptorpos[1]] = "R"
 
 	def printgrid(self):
 		for i in self.TheGrid:
 			print("   " + str(i).replace(",","").replace("[","").replace("]","").replace("\'",""))
 	def moveRaptor(self,direction):
 		for i in direction:
-			if direction == "U":
-				value_x=raptorpos[0]
-				value_y=raptorpos[1]
-				raptorpos=[value_x-1,value_Y]
-				self.TheGrid[raptorpos[0]][raptorpos[1]] = "R"
-				self.printgrid()
+			if i == "U":
+
+				value_x=self.raptorpos[0]
+				value_y=self.raptorpos[1]
+				self.raptorpos=[value_x-1,value_y]
+				self.TheGrid[value_x][value_y] = "."
+				if self.TheGrid[self.raptorpos[0]][self.raptorpos[1]] == "S":
+					print(">>> YOU WON!\nYou ate the Swine!")
+					exit()
+				elif self.TheGrid[self.raptorpos[0]][self.raptorpos[1]] == "T":
+					print(">>> YOU LOST!\nYou ate a Trinitrotoluene")
+					exit()
+				else:
+					self.TheGrid[self.raptorpos[0]][self.raptorpos[1]] = "R"
+			if i == "D":
+				value_x=self.raptorpos[0]
+				value_y=self.raptorpos[1]
+				self.raptorpos=[value_x+1,value_y]
+				self.TheGrid[value_x][value_y] = "."
+				if self.TheGrid[self.raptorpos[0]][self.raptorpos[1]] == "S":
+					print(">>> YOU WON!\nYou ate the Swine!")
+					exit()
+				elif self.TheGrid[self.raptorpos[0]][self.raptorpos[1]] == "T":
+					print(">>> YOU LOST!\nYou ate a Trinitrotoluene")
+					exit()
+				else:
+					self.TheGrid[self.raptorpos[0]][self.raptorpos[1]] = "R"
+			if i == "R":
+				value_x=self.raptorpos[0]
+				value_y=self.raptorpos[1]
+				self.raptorpos=[value_x,value_y+1]
+				self.TheGrid[value_x][value_y] = "."
+				if self.TheGrid[self.raptorpos[0]][self.raptorpos[1]] == "S":
+					print(">>> YOU WON!\nYou ate the Swine!")
+					exit()
+				elif self.TheGrid[self.raptorpos[0]][self.raptorpos[1]] == "T":
+					print(">>> YOU LOST!\nYou ate a Trinitrotoluene")
+					exit()
+				else:
+					self.TheGrid[self.raptorpos[0]][self.raptorpos[1]] = "R"
+			if i == "L":
+				value_x=self.raptorpos[0]
+				value_y=self.raptorpos[1]
+				self.raptorpos=[value_x,value_y-1]
+				self.TheGrid[value_x][value_y] = "."
+				if self.TheGrid[self.raptorpos[0]][self.raptorpos[1]] == "S":
+					print(">>> YOU WON!\nYou ate the Swine!")
+					exit()
+				elif self.TheGrid[self.raptorpos[0]][self.raptorpos[1]] == "T":
+					print(">> YOU LOST!\nYou ate a Trinitrotoluene")
+					exit()
+				else:
+					self.TheGrid[self.raptorpos[0]][self.raptorpos[1]] = "R"
 grid = Grid()
 raptor = Raptor()
 grid.printgrid()
 while raptor.Lost == False:
 	moves=int(input(">>> How many moves: "))
 	for _ in range(moves):
-		raptor.moves(input(">>> Move L/R/U/D: ").upper())
-		raptor.moves()
-	grid.moveRaptor(raptor.moves())
+		raptor.addMoves(input(">>> Move L/R/U/D: ").upper())
+	print(">>> Moves: " + str(raptor.getMoves()).replace("[","").replace("\'","").replace(",","").replace("]",""))
+	grid.moveRaptor(raptor.getMoves())
+	grid.printgrid()
+	raptor.clearMoves()
