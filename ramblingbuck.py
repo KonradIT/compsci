db42f898bbd82b25ff4a4156848f79dd1f82751a
Konrad Iturbe <KonradIT@users.noreply.github.com>
2017-11-10 11:45:31 +0100

    Update ramblingbuck.py

--- ramblingbuck.py
+++ ramblingbuck.py
@@ -4,18 +4,10 @@ import time
 class RamblingBuck:
 	def __init__(self):
 		self.battlefield =[[" " for x in range(2)] for y in range(5)]	
-		self.battlefield[0][0] = "B"
-		self.battlefield[0][1] = "B"
+		self.battlefield[0][0] = "1"
+		self.battlefield[0][1] = "2"
 		self.positions=[0,0]
 		self.lost=False
-		print("  _____                 _     _ _               ____             _    ")
-		print(" |  __ \               | |   | (_)             |  _ \           | |   ")
-		print(" | |__) |__ _ _ __ ___ | |__ | |_ _ __   __ _  | |_) |_   _  ___| | __")
-		print(" |  _  // _` | '_ ` _ \| '_ \| | | '_ \ / _` | |  _ <| | | |/ __| |/ /")
-		print(" | | \ \ (_| | | | | | | |_) | | | | | | (_| | | |_) | |_| | (__|   < ")
-		print(" |_|  \_\__,_|_| |_| |_|_.__/|_|_|_| |_|\__, | |____/ \__,_|\___|_|\_\")
-		print("                                         __/ |                        ")
-		print("                                        |___/                         ")
 	def printbattlefield(self):
 		for i in self.battlefield:
 			print(str(i).replace("[","").replace("]","").replace(",",""))
@@ -24,20 +16,21 @@ class RamblingBuck:
 		indicator=0
 		opponent=0
 		if channel == 1:
-			indicator=1
-			opponent=0
-		else:
-			indicator=0
 			opponent=1
-		if self.battlefield[self.positions[opponent]][opponent] == "X":
-			self.positions[opponent]=self.positions[opponent]+1
-			self.battlefield[self.positions[opponent]][opponent] = B"
 		else:
-			self.battlefield[self.positions[opponent]][opponent] = "X"
-		self.battlefield[self.positions[opponent]][indicator] = " "
-		self.battlefield[self.positions[opponent]+1][indicator] = "B"
-		self.positions[indicator]=self.positions[indicator]+1
-		self.positions[opponent]=self.positions[opponent]+1
+			indicator=1
+		
+		self.battlefield[self.positions[indicator]][indicator] = " "
+		self.battlefield[self.positions[opponent]][opponent] = " "
+		self.battlefield[self.positions[opponent]+1][opponent] = str(opponent+1)
+		self.positions[indicator] += 1
+		self.positions[opponent] += 1
+		if self.battlefield[self.positions[indicator]][indicator] == "X":
+			self.positions[indicator] = 0
+			self.battlefield[self.positions[indicator]+1][indicator] = " "
+			self.battlefield[self.positions[indicator]][indicator] = str(channel)
+		else:
+			self.battlefield[self.positions[indicator]+1][indicator] = "X"
 bucks = RamblingBuck()
 while bucks.lost == False:
 	bucks.printbattlefield()
