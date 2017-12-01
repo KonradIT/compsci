463d7e738542ba993049ec9e91e1998e180c6dfb
Konrad Iturbe <KonradIT@users.noreply.github.com>
2017-12-01 16:37:09 +0100

    Update renfe_api.py

--- renfe/renfe_api.py
+++ renfe/renfe_api.py
@@ -1,9 +1,16 @@
 #API based on horarios app from Android. 
 
+"""
+TODO
+
+Fix remaining time till departure
+
+"""
+
 import urllib.request
 import datetime
 import re
-
+import time
 class Renfe:
 	def __init__(self, nucleo, origen, destino):
 		now = datetime.datetime.now()
@@ -22,24 +29,40 @@ class Renfe:
 		self.html_src = urllib.request.urlopen(self.url).read().decode('utf-8','ignore').encode("utf-8")
 		return self.html_src
 		
-	def get_table(self, src):
+	def get_table(self, src, remaining=False):
 		#print(src)
 		#Get HTML table: <table border="0" width="95%" cellpadding="1" cellspacing="1" align="center" id="tabla" class="horarios"> and </table>
 		start= "<table border=\"0\" width=\"95%\" cellpadding=\"1\" cellspacing=\"1\" align=\"center\" id=\"tabla\" class=\"horarios\">"
 		end="</table>"
+		now = datetime.datetime.now()
 		table = src.split(start)[1].split(end)[0]
 		#print(table)
 		#Iteration to print a decent ascii table.
 		for index, line in enumerate(src.split('\n')):
 			if "<tr class" in line:
-				self.tabla_horarios.append([src.splitlines()[index+2].strip(), src.splitlines()[index+9].replace("<td align=center>","").replace("</td>","").strip(), src.splitlines()[index+10].replace("<td align=center>","").replace("</td>","").strip()])
+				if remaining:
+					departure=src.splitlines()[index+9].replace("<td align=center>","").replace("</td>","").strip()
+					if now.strftime("%H.%M") < departure:
+						pass
+					diff = total_time=(datetime.datetime.strptime(departure,'%H.%M') - datetime.datetime.strptime(now.strftime("%H.%M"),'%H.%M'))
+					self.tabla_horarios.append([src.splitlines()[index+2].strip(), departure + " - " + str(diff), src.splitlines()[index+10].replace("<td align=center>","").replace("</td>","").strip()])
+				else:
+					self.tabla_horarios.append([src.splitlines()[index+2].strip(), src.splitlines()[index+9].replace("<td align=center>","").replace("</td>","").strip(), src.splitlines()[index+10].replace("<td align=center>","").replace("</td>","").strip()])
 		print("|Linea|  Dep  |Arrival|")
 		for i in self.tabla_horarios:
 			
 			line = str(i).replace("[","| ").replace("]"," |").replace("\'","").replace(","," |")
 			print(line)
 			print("-"*len(line))
-		
+	def get_next_train(self, src):
+		start= "<table border=\"0\" width=\"95%\" cellpadding=\"1\" cellspacing=\"1\" align=\"center\" id=\"tabla\" class=\"horarios\">"
+		end="</table>"
+		table = src.split(start)[1].split(end)[0]
+		for index, line in enumerate(src.split('\n')):
+			if "<tr class" in line:
+				next_train=  [src.splitlines()[index+2].strip(), src.splitlines()[index+9].replace("<td align=center>","").replace("</td>","").strip(), src.splitlines()[index+10].replace("<td align=center>","").replace("</td>","").strip()]
+				break
+		return next_train
 alpedrete_villalba = Renfe("10","10200","12002")# Madrid, Alpedrete > Villalba: 10, 10200 12002
 now = datetime.datetime.now()
 #print(alpedrete_villalba.get_trains())
@@ -47,4 +70,4 @@ print("Renfe cercanias\nAlpedrete to Villalba")
 if input("Full table or starting from " + now.strftime("%H hour") + "?: [full/now]: ").lower() == "full":
 	alpedrete_villalba.get_table(alpedrete_villalba.get_trains().decode("utf-8"))
 else:
-	alpedrete_villalba.get_table(alpedrete_villalba.get_from_time(now.strftime("%H")).decode("utf-8"))
+	alpedrete_villalba.get_table(alpedrete_villalba.get_from_time(now.strftime("%H")).decode("utf-8"), remaining=True)
