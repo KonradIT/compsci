995d37151c6a7d5944bb79eb749e9a8d920c8783
Konrad Iturbe <KonradIT@users.noreply.github.com>
2017-12-01 11:44:50 +0100

    Update renfe_api.py

--- renfe/renfe_api.py
+++ renfe/renfe_api.py
@@ -2,7 +2,8 @@
 
 import urllib.request
 import datetime
-from html.parser import HTMLParser
+import re
+
 class Renfe:
 	def __init__(self, nucleo, origen, destino):
 		now = datetime.datetime.now()
@@ -10,8 +11,8 @@ class Renfe:
 		self.origen = origen
 		self.destino = destino
 		self.url = 'http://horarios.renfe.com/cer/hjcer310.jsp?nucleo={}&i=i&cp=NO&o={}&d={}&df={}&ho=00&hd=26&TXTInfo='.format(nucleo,origen,destino,now.strftime("%Y%m%d"))  #Madrid, Alpedrete > Villalba: 10, 10200 12002
-		self.html_src = urllib.request.urlopen(self.url).read()
-		
+		self.html_src = urllib.request.urlopen(self.url).read().decode('utf-8','ignore').encode("utf-8")
+		self.tabla_horarios=[]
 	def get_trains(self):
 		return self.html_src
 	
@@ -22,9 +23,18 @@ class Renfe:
 		return self.html_src
 		
 	def get_table(self, src):
-		parser = HTMLParser()
-		parser.feed(src)	
+		#print(src)
+		#Get HTML table: <table border="0" width="95%" cellpadding="1" cellspacing="1" align="center" id="tabla" class="horarios"> and </table>
+		start= "<table border=\"0\" width=\"95%\" cellpadding=\"1\" cellspacing=\"1\" align=\"center\" id=\"tabla\" class=\"horarios\">"
+		end="</table>"
+		table = src.split(start)[1].split(end)[0]
+		#print(table)
+		#Iteration to print a decent ascii table.
+		for index, line in enumerate(src.split('\n')):
+			if "<tr class" in line:
+				self.tabla_horarios.append([src.splitlines()[index+2], src.splitlines()[index+9].replace("<td align=center>","").replace("</td>","")])
 	
+		print(self.tabla_horarios)
 alpedrete_villalba = Renfe("10","10200","12002")
 #print(alpedrete_villalba.get_trains())
-alpedrete_villalba.get_table(alpedrete_villalba.get_trains())
+alpedrete_villalba.get_table(alpedrete_villalba.get_trains().decode("utf-8"))
