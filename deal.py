1700b4626816dc8c7745491b1b24fbd03b354326
Konrad Iturbe <KonradIT@users.noreply.github.com>
2017-12-22 11:38:52 +0100

    Update deal.py

--- deal.py
+++ deal.py
@@ -1,6 +1,7 @@
 #Deal|!Deal game
 
 import random
+
 class DealOrNoDeal:
 	def __init__(self):
 		self.moneys=["0.1","0.10","0.50","1","5","10","50","100","250","500","750","1000","3000","5000","10000","15000","20000","35000","50000","75000","100000","250000"]
@@ -9,15 +10,26 @@ class DealOrNoDeal:
 		self.money = random.sample( self.moneys, len(self.moneys) )
 		for index,i in enumerate(self.boxes):
 			i.append(self.money[index])
-		print(self.boxes)
-		print("====== WELCOME TO DEAL|!DEAL ======\n>>> Please choose a box.")
+		print("====== WELCOME TO DEAL|!DEAL ======\nInstructions: Attempt to guess the boxes with the least money so the final box has 250k. Bank will make an offer in order to pay less\nEnter a box for you to keep\n")
 		print("| ",end="")
 		for i in self.boxes:
 			print(i[0],end=" | ")
 		self.playerbox=int(input("\n>>> $ "))
-		self.startGame()
+		self.lost = False
 	def startGame(self):
-		print("Prizes available:")
+		if len(self.boxes) == 1:
+			self.lost = True
+		print("| ",end="")
+		for i in self.boxes:
+			print(i[0],end=" | ")
+		print("\nPrizes available:")
 		print(str(self.moneytodisplay).replace("[","").replace("]","").replace(",","$").replace("\'",""),end="$\n")
 		print("OK Player, enter a box: ")
+		box=int(input(">>> # "))
+		print("Box " + str(box+1) + " has " + self.boxes[box][1] + "$\n\n")
+		self.moneytodisplay.remove(str(self.boxes[box][1]))
+		del self.boxes[box]
+		
 dond = DealOrNoDeal()
+while dond.lost == False:
+	dond.startGame()
