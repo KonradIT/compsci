ba5a98768b41a13f7bdec84c107dafa39e890368
Konrad Iturbe <KonradIT@users.noreply.github.com>
2018-01-12 09:53:07 +0100

    Create bigbreak.py

---
+++ bigbreak.py
@@ -0,0 +1,19 @@
+## Big Break 
+
+class BigBreak:
+	def __init__(self):
+		self.questions=[
+						[
+							["Whats the capital of sweden","stockholm"],
+							["Whats the capital of spain","madrid"],
+							["largest river...","amazon"]
+						],
+						[
+							["Who won the US Presidential election in 2016?","Donald Trump"],
+							["In what year was YouTube founded?","2005"]
+							["Where was the first iPhone unveiled?","San Francisco"]
+							["What is the capital of Uganda?", "Kampala"]
+							["What is 38^7-100 ?","114415582492"]
+						]
+						]
+		self.shots=["R,"R","R","G,"G","G","C,"C","C","B,"B","B"]
